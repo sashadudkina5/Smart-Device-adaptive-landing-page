aef26f76c0e37b17d4245d0f9a0ab5ed80b8ebcc
sashadudkina5 <uu.aaa@mail.ru>
2023-04-28 09:53:21 +0400

    fixed

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -1,6 +1,11 @@
 const requestCallButton = document.querySelector('[data-action="request-call-modal"]');
 const modalWindow = document.querySelector('[data-modal="feedback"]');
 const closeModalButton = document.querySelector('[data-action="close-modal"]');
+const modalContainer = document.querySelector('.modal__container');
+
+const isEscapeKey = (evt) => {
+  return evt.key === 'Escape';
+};
 
 const openModal = (modal) => {
   modal.classList.add('is-active');
@@ -9,15 +14,37 @@ const openModal = (modal) => {
   const closeModal = () => {
     modal.classList.remove('is-active');
     modal.classList.add('is-closed');
+    document.removeEventListener('keydown', onUploaderEscapeKeydown);
+    document.removeEventListener('click', onOutsideModalClick);
   };
 
   closeModalButton.addEventListener('click', () => {
     closeModal();
   });
+
+  const onUploaderEscapeKeydown = (evt) => {
+    if (isEscapeKey(evt)) {
+      evt.preventDefault();
+      closeModal();
+    }
+  };
+
+  const onOutsideModalClick = (evt) => {
+    if (modal.classList.contains('is-active') &&
+        evt.target !== modalContainer &&
+        evt.target !== requestCallButton &&
+        !modalContainer.contains(evt.target)) {
+      closeModal();
+    }
+  };
+
+  document.addEventListener('keydown', onUploaderEscapeKeydown);
+  document.addEventListener('click', onOutsideModalClick);
 };
 
 const toggleModal = () => {
-  requestCallButton.addEventListener('click', () => {
+  requestCallButton.addEventListener('click', (event) => {
+    event.stopPropagation();
     openModal(modalWindow);
   });
 };
